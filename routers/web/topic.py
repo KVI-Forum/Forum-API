a2744296691aa1604ab700f7c329c18726dface3
ivanivan <izlatinov19@gmail.com>
2024-11-07 17:23:49 +0200

    fixed endpoint and removed messenger

--- routers/web/topic.py
+++ routers/web/topic.py
@@ -3,7 +3,7 @@ from common.template_config import CustomJinja2Templates
 from services import topic_service,user_service,reply_service
 from fastapi.responses import RedirectResponse
 from common.auth import verify_authenticated_user
-topic_router = APIRouter(prefix='/topic')
+topic_router = APIRouter(prefix='/topics')
 templates = CustomJinja2Templates(directory='templates')
 
 @topic_router.get('/{topic_id}')
@@ -27,23 +27,21 @@ def get_topic_by_id(request: Request, topic_id: int):
     return templates.TemplateResponse("topic.html", {"request": request, "topic": topic, "replies": replies, "access": access, "upvotes": upvotes, "downvotes": downvotes, "topic_author": topic_author, "topic_id": topic_id })
 
 
-@topic_router.post("/{topic_id}/reply")
+@topic_router.post("/{topic_id}/replies")
 def post_reply(
     topic_id: int,
     request: Request,
     content: str = Form(...),
     token: str = Form(...)
 ):
-    # Extract user_id from token
+   
     user_id = int(token.split(";")[0])
     verify_authenticated_user(token)
-    
-    # Call your reply service to create a reply
+
     reply_id = reply_service.create(content, topic_id, user_id)
     
     if reply_id:
-        # formatted_created_at = create_datetime.strftime('%Y-%m-%d %H:%M:%S')
-        return RedirectResponse(f'/topic/{topic_id}',status_code=302)
+        return RedirectResponse(f'/topics/{topic_id}',status_code=302)
         
     else:
         return templates.TemplateResponse("fail.html", {"request": request})
